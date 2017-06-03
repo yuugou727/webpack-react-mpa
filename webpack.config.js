a9adc050e102d81ba52ff7e1fcc6f759148e833b
Ronnie Chang <yuugou727@gmail.com>
2017-06-03 15:49:29 +0800

    webpack config vendor and manifest

--- webpack.config.js
+++ webpack.config.js
@@ -37,10 +37,21 @@ var config = {
 		] 
 	},
 	plugins: [ 
+		// new webpack.optimize.CommonsChunkPlugin({
+        //     name: ['vendor','manifest'], // 'vendor' keeps long-term cache. split runtime code into 'manifest'
+        //     minChunks: Infinity,
+        // }),
 		new webpack.optimize.CommonsChunkPlugin({
-            name: ['vendor','manifest'], // 'vendor' keeps long-term cache. split runtime code into 'manifest'
-            minChunks: Infinity,
-        }),
+			name: 'vendor',
+			minChunks: function (module) {
+				// this assumes your vendor imports exist in the node_modules directory
+				return module.context && module.context.indexOf('node_modules') !== -1;
+			}
+		}),
+		//CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+		new webpack.optimize.CommonsChunkPlugin({ 
+			name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+		})
 		new HtmlWebpackPlugin({
 			title: 'My App',
 			favicon: '',
